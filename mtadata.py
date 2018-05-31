be9382f379a741e92fbd2453119c0d90747838a9
Clara Mohri <claramohri@napoleon.fios-router.home>
2018-05-31 19:53:01 -0400

    Initial data processing

---
+++ mtadata.py
@@ -0,0 +1,55 @@
+file = open("mtainfo.csv", "r")
+csv = file.readlines()
+file.close()
+
+print (csv[:10])            
+            
+def splitCommas(list):
+    ctr = 0
+    while ctr < len(list):
+        list[ctr] = list[ctr].split(", ")
+        ctr += 1
+    ctr = 0
+    while ctr < len(list):
+        list[ctr] = list[ctr][0].split(",")
+        ctr += 1        
+    return list
+
+splitCommas(csv)
+print ("split commas")
+print(csv[:10])
+
+def rmWhiteSp(list):
+    ctr = 0
+    while ctr < len(list):
+        subctr = 0
+        while subctr < len(list[ctr]):
+            list[ctr][subctr] = list[ctr][subctr].strip()
+            subctr += 1
+        ctr += 1
+    return list
+
+rmWhiteSp(csv)
+print("rm white space: ")
+print(csv[:10])
+
+def rmCol(num):
+    ctr = 0
+    while ctr < len(csv):
+        csv[ctr] = csv[ctr][0:num]+ csv[ctr][num + 1:]
+        ctr += 1
+
+rmCol(0)
+rmCol(0)
+rmCol(0)
+rmCol(0)
+rmCol(0)
+rmCol(3)
+print(csv[:5])
+
+for i in csv:
+    try: 
+        if i[2].index("6") >= 0:
+            print i
+    except:
+        pass
