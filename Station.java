e6fea3b53b5d107016300c9cf91a350172f58adc
Clara Mohri <claramohri@napoleon.local>
2018-06-01 13:36:06 -0400

    Update Station.java

--- Station.java
+++ Station.java
@@ -9,20 +9,20 @@ public class Station{
     String _line; //could be number of letter, but as a String
     double _xcor;
     double _ycor;
-    boolean _terminus; //true if it is the endpoint of a train line
-    int _borough; /*boroughs: 
-		   1: Brooklyn
-		   2: Bronx
-		   3: Manhattan
-		   4: Queens
-		   (Staten Island not included)*/
+    //boolean _terminus; //true if it is the endpoint of a train line
+    String _borough; /*boroughs: 
+		       Bx
+		       M
+		       Q
+		       Bk
+		       (Staten Island not included)*/
     //constructor
-    public Station(String name, String line, double xcor, double ycor, boolean terminus, int borough){
+    public Station(String name, String borough, String line, double xcor, double ycor){
 	_name = name;
 	_line = line;
 	_xcor = xcor;
 	_ycor = ycor;
-	_terminus = terminus;
+	//_terminus = terminus;
 	_borough = borough;
     }
     //accessors: 
@@ -32,10 +32,19 @@ public class Station{
     public String getLine(){
 	return _line;
     }
-    public boolean isTerminus(){
+    /*    public boolean isTerminus(){
 	return _terminus;
-    }
-   
+	}*/
 
+
+    public static void main (String[] args){
+
+	Station PBP = new Station("Pelham Bay Park", "Bx", "6", Double.parseDouble("40.852462"), Double.parseDouble("-73.828121"));   
+	Station B = new Station("Buhre Av", "Bx", "6", Double.parseDouble("40.84681"), Double.parseDouble("-73.832569"));
+	System.out.println(PBP.getName());
+	System.out.println(PBP.getLine());
+
+	
+    }
 	
 }
