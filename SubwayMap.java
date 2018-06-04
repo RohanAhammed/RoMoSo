62ebcdba3ba4742f922a05932097930a588c5fb0
Rohan Ahammed <rahammed@stuy.edu>
2018-06-04 00:40:21 -0400

    added to driver

--- SubwayMap.java
+++ SubwayMap.java
@@ -5,8 +5,12 @@ public class SubwayMap{
 	System.out.println("Welcome to the interactive SubwayMap, a program that tells you the fastest route between 2 MTA train stations");
 	System.out.println("Enter your departure station: ");
 	String depStation = Keyboard.readString();
+	//code to find depStation object
+	//String deptLine = Keyboard.readString();//might be unnecessary
 	System.out.println("Enter your destination station: ");
 	String destStation = Keyboard.readString();
+      	//code to find destStation object
+
 	System.out.println("Finding the shortest route . . .");
     }
 }//end of public class
